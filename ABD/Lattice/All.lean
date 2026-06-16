44a615e8b19a2aabfeb3c326724fac6e32555b46
sakaguchi5 <sokyu_yokyu@ezweb.ne.jp>
2026-06-16 14:49:38 +0900

    単なる Setながらzero・add・neg・整数スカラー倍で閉じる

--- ABD/Lattice/All.lean
+++ ABD/Lattice/All.lean
@@ -1,7 +1,11 @@
-/- Copyright (c) 2026 NOIZ. All rights reserved. Released under Apache 2.0 license as described in the file LICENSE.
-Authors: NOIZ -/
+/-
+Copyright (c) 2026 NOIZ. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: NOIZ
+-/
 import ABD.Lattice.Kernel
 import ABD.Lattice.AdditiveLinearFormLinear
+import ABD.Lattice.KernelClosure
 import ABD.Lattice.Smallness
 import ABD.Lattice.SmallnessAbs
 import ABD.Lattice.SmallnessWeighted
