4bdb8650c96575ef2d5158e62819c20c56c98721
sakaguchi5 <sokyu_yokyu@ezweb.ne.jp>
2026-06-17 08:36:20 +0900

    続き

--- ABD/Examples/All.lean
+++ ABD/Examples/All.lean
@@ -1,7 +1,8 @@
-/- Copyright (c) 2026 NOIZ. All rights reserved. Released under Apache 2.0 license as described in the file LICENSE.
+/- Copyright (c) 2026 NOIZ. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
 Authors: NOIZ -/
-
 import ABD.Examples.TwoThreeFive
 import ABD.Examples.PrimePowerToy
 import ABD.Examples.PastenTwoThreeFive
 import ABD.Examples.ConcreteKernel235
+import ABD.Examples.PastenConcrete235
